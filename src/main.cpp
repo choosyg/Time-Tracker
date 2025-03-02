189185d0e6662cbd42ae4fd8908acb4cc9b13773
Stefan Schiffler <choosyg@gmail.com>
2025-03-02 22:33:02 +0100

    Added inital Code for e-Paper Display

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,102 @@
 
 #include <Arduino_LSM9DS1.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <SPI.h>
+#include "epd1in54.h"
+#include "epdpaint.h"
+#include "imagedata.h"
+
+#define COLORED     0
+#define UNCOLORED   1
+unsigned char image[1024];
+Paint paint(image, 0, 0);    // width should be the multiple of 8 
+Epd epd;
+unsigned long time_start_ms;
+unsigned long time_now_s;
 
 void setup() {
   Serial.begin(115200);    // initialize serial communication
 
-  // begin initialization
+  Serial.print("IMU init ");
   if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU!");
     while (1);
   }
+
+  Serial.print("e-Paper init ");
+  if (epd.Init(lut_full_update) != 0) {
+      Serial.print("e-Paper init failed");
+      return;
+  }
+
+  /** 
+   *  there are 2 memory areas embedded in the e-paper display
+   *  and once the display is refreshed, the memory area will be auto-toggled,
+   *  i.e. the next action of SetFrameMemory will set the other memory area
+   *  therefore you have to clear the frame memory twice.
+   */
+  epd.ClearFrameMemory(0xFF);   // bit set = white, bit reset = black
+  epd.DisplayFrame();
+  epd.ClearFrameMemory(0xFF);   // bit set = white, bit reset = black
+  epd.DisplayFrame();
+
+  paint.SetRotate(ROTATE_0);
+  paint.SetWidth(200);
+  paint.SetHeight(24);
+
+  /* For simplicity, the arguments are explicit numerical coordinates */
+  paint.Clear(COLORED);
+  paint.DrawStringAt(30, 4, "Hello world!", &Font16, UNCOLORED);
+  epd.SetFrameMemory(paint.GetImage(), 0, 10, paint.GetWidth(), paint.GetHeight());
+  
+  paint.Clear(UNCOLORED);
+  paint.DrawStringAt(30, 4, "e-Paper Demo", &Font16, COLORED);
+  epd.SetFrameMemory(paint.GetImage(), 0, 30, paint.GetWidth(), paint.GetHeight());
+
+  paint.SetWidth(64);
+  paint.SetHeight(64);
+  
+  paint.Clear(UNCOLORED);
+  paint.DrawRectangle(0, 0, 40, 50, COLORED);
+  paint.DrawLine(0, 0, 40, 50, COLORED);
+  paint.DrawLine(40, 0, 0, 50, COLORED);
+  epd.SetFrameMemory(paint.GetImage(), 16, 60, paint.GetWidth(), paint.GetHeight());
+
+  paint.Clear(UNCOLORED);
+  paint.DrawCircle(32, 32, 30, COLORED);
+  epd.SetFrameMemory(paint.GetImage(), 120, 60, paint.GetWidth(), paint.GetHeight());
+
+  paint.Clear(UNCOLORED);
+  paint.DrawFilledRectangle(0, 0, 40, 50, COLORED);
+  epd.SetFrameMemory(paint.GetImage(), 16, 130, paint.GetWidth(), paint.GetHeight());
+
+  paint.Clear(UNCOLORED);
+  paint.DrawFilledCircle(32, 32, 30, COLORED);
+  epd.SetFrameMemory(paint.GetImage(), 120, 130, paint.GetWidth(), paint.GetHeight());
+  epd.DisplayFrame();
+
+  delay(2000);
+  
+  
+
+  if (epd.Init(lut_partial_update) != 0) {
+      Serial.print("e-Paper init failed");
+      return;
+  }
+
+  /** 
+   *  there are 2 memory areas embedded in the e-paper display
+   *  and once the display is refreshed, the memory area will be auto-toggled,
+   *  i.e. the next action of SetFrameMemory will set the other memory area
+   *  therefore you have to set the frame memory and refresh the display twice.
+   */
+  epd.SetFrameMemory(IMAGE_DATA);
+  epd.DisplayFrame();
+  epd.SetFrameMemory(IMAGE_DATA);
+  epd.DisplayFrame();
+
+  time_start_ms = millis();
+  //epd.Sleep();
 }
 
 void loop() {
@@ -36,9 +121,21 @@ void loop() {
   } else {
     //Serial.println("ERROR: No Accelerometer");
   }
-}
+  time_now_s = (millis() - time_start_ms) / 1000;
+  char time_string[] = {'0', '0', ':', '0', '0', '\0'};
+  time_string[0] = time_now_s / 60 / 10 + '0';
+  time_string[1] = time_now_s / 60 % 10 + '0';
+  time_string[3] = time_now_s % 60 / 10 + '0';
+  time_string[4] = time_now_s % 60 % 10 + '0';
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  paint.SetWidth(32);
+  paint.SetHeight(96);
+  paint.SetRotate(ROTATE_270);
+
+  paint.Clear(UNCOLORED);
+  paint.DrawStringAt(0, 4, time_string, &Font24, COLORED);
+  epd.SetFrameMemory(paint.GetImage(), 80, 72, paint.GetWidth(), paint.GetHeight());
+  epd.DisplayFrame();
+
+  delay(500);
 }
